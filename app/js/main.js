7c8aa215b7361d0ed58c81bf96fded36dedb526f
alex <electrod.alex@i.ua>
2024-02-07 14:59:54 +0300

    beautify main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,17 @@
 const menuBox = document.querySelector('.nav');
 const menuTogglerBtn = document.querySelector('.menu-toggler-btn');
 
-menuTogglerBtn.addEventListener('click', ()=>{
-menuTogglerBtn.classList.toggle('active');
-menuBox.classList.toggle('active');
+menuTogglerBtn.addEventListener('click', () => {
+  menuTogglerBtn.classList.toggle('active');
+  menuBox.classList.toggle('active');
 });
 
-menuTogglerBtn.addEventListener('keyup', (e)=>{
-  if(e.key === 'Enter' && !menuTogglerBtn.classList.contains('active')){
+menuTogglerBtn.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter' && !menuTogglerBtn.classList.contains('active')) {
     menuTogglerBtn.classList.toggle('active');
     menuBox.classList.toggle('active');
   }
-  if(e.key === 'Escape' && menuTogglerBtn.classList.contains('active')){
+  if (e.key === 'Escape' && menuTogglerBtn.classList.contains('active')) {
     menuTogglerBtn.classList.remove('active');
     menuBox.classList.remove('active');
   }
@@ -22,13 +22,13 @@ $(document).ready(function () {
     infinite: true,
     arrows: false,
   });
-  
+
   $('.small-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     infinite: true,
     prevArrow: '<button class="small-slider-arrow small-alider-prev" aria-label="prev"><i class="fa-solid fa-chevron-left"></i></button>',
-  nextArrow: '<button class="small-slider-arrow small-slider-next" aria-label="next"><i class="fa-solid fa-chevron-right"></i></button>',
+    nextArrow: '<button class="small-slider-arrow small-slider-next" aria-label="next"><i class="fa-solid fa-chevron-right"></i></button>',
     asNavFor: '.pancakes-big-slider',
   });
 });
