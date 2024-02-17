76399e02b2837998ab18c3492d9af4b169d58735
alex <electrod.alex@i.ua>
2024-02-17 14:46:09 +0300

    remove commented lines from js

--- app/js/main.js
+++ app/js/main.js
@@ -73,26 +73,7 @@ $(document).ready(function () {
     $("#dp-dots li").removeClass("active");
     $("#dp-dots li[data-class=" + get_active + "]").addClass("active");
   }
-  /*$("#dp-next").click(function () {
-    var total = $(".dp_item").length;
-    $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
-    $.each($('.dp_item'), function (index, dp_item) {
-      $(dp_item).attr('data-position', index + 1);
-    });
-    detect_active();
-
-  });
-
-  $("#dp-prev").click(function () {
-    var total = $(".dp_item").length;
-    $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
-    $.each($('.dp_item'), function (index, dp_item) {
-      $(dp_item).attr('data-position', index + 1);
-    });
-
-    detect_active();
-  });
-*/
+  
   $("#dp-dots li").click(function () {
     $("#dp-dots li").removeClass("active");
     $(this).addClass("active");
@@ -103,7 +84,6 @@ $(document).ready(function () {
       $(dp_item).attr('data-position', index + 1);
     });
   });
-
 
   $("body").on("click", "#dp-slider .dp_item:not(:first-child)", function () {
     var get_slide = $(this).attr('data-class');
